e575a839cfe912849b4260d153576942410f6345
khalid hasan <khalidhasan9953@gmail.com>
2026-07-22 22:22:21 +0600

    vendor layout done

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -4,7 +4,7 @@ import DashboardSidebar from '@/components/dashboard/DashboardSidebar';
 export default async function DashboardLayout({ children }) {
     const user = await requireLogin();
 
-    return (
+    return ( 
         <div className="mx-auto max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
             <div className="flex flex-col gap-8 lg:flex-row">
                 <DashboardSidebar role={user.role} />
