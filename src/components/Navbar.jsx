938b9802e32b9656f5e99bb2aeb9b7beba0bbd9a
khalid hasan <khalidhasan9953@gmail.com>
2026-07-22 22:19:07 +0600

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import Link from 'next/link';
+import Link from 'next/link'; 
 import { usePathname, useRouter } from 'next/navigation';
 import { Bus, Menu, X, Moon, Sun } from 'lucide-react';
 import { useSession, signOut } from '@/lib/auth-client';
